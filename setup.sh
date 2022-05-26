7628d089852f997cc468e7596674b413c16d6300
Robert Kelley <rrkelley@pop-os.localdomain>
2022-05-26 12:25:05 -0400

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
+
+# Images to Pull
+
+
+# Images to Start
+
+# Install/Start MySQL (1 or 2 machines)
+
